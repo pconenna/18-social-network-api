a750a0c7410cbd7b02a444cc9f97a119d9e9db69
Peter Conenna <pconenna@depaul.edu>
2023-08-09 16:25:18 -0500

    almost complete functionality

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -11,3 +11,5 @@ const{
 router.route('/').get(getAllThoughts).post(createThought)
 router.route('/:thoughtId').get(getOneThought).put(updateThought).delete(deleteThought)
 router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction)
+
+module.exports = router;
